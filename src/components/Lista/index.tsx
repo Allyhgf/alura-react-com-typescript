4d19e7ccfabd3798d084589f14a34bd7b2592cf1
Allyson Hansel <allysongithub21@gmail.com>
2022-10-05 20:42:47 -0300

    Componentização

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,33 @@
+import { stringify } from "querystring";
+import React from "react";
+
+export function Lista() {
+  const tarefas = [
+    {
+      tarefa: "React",
+      tempo: "02:00:00",
+    },
+    {
+      tarefa: "Javascript",
+      tempo: "01:00:00",
+    },
+    {
+      tarefa: "Typescript",
+      tempo: "00:30:00",
+    },
+  ];
+
+  return (
+    <aside>
+      <h2>Estudos do dia</h2>
+      <ul>
+        {tarefas.map((item, index) => (
+          <li key={index}>
+            <h3>{item.tarefa}</h3>
+            <span>{item.tempo}</span>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+}
