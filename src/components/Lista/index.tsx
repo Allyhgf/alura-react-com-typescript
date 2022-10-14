d50da07732510ae693766e893e7ab19592996eb8
Allyson Hansel <allysongithub21@gmail.com>
2022-10-14 18:47:54 -0300

    Gerenciando state e adicionando itens na lista

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,29 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 import style from "./Lista.module.scss";
 
 export default function Lista() {
-  const tarefas = [
+  const [tarefas, setTarefas] = useState([
     {
       tarefa: "React",
       tempo: "02:00:00",
     },
     {
       tarefa: "Javascript",
-      tempo: "01:00:00",
+      tempo: "01:30:00",
     },
     {
       tarefa: "Typescript",
-      tempo: "00:30:00",
+      tempo: "03:00:00",
     },
-  ];
-
+  ]);
   return (
-    <aside>
-      <h2>Estudos do dia</h2>
+    <aside className={style.listaTarefas}>
+      <h2
+        onClick={() => {
+          console.log("Click no H2", tarefas);
+          setTarefas([
+            ...tarefas,
+            {
+              tarefa: "Estudar estado",
+              tempo: "05:00:00",
+            },
+          ]);
+        }}
+      >
+        Estudos do dia
+      </h2>
       <ul>
         {tarefas.map((item, index) => (
-          <Item key={index} {...item}  />
+          <Item key={index} {...item} />
         ))}
       </ul>
     </aside>
